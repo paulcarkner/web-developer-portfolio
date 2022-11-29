f7183805f0dee2375c75b60d5ea0ee82f21aa50a
paulcarkner <paul.carkner@gmail.com>
2022-11-28 21:35:28 -0500

    Updated html, body css loading to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import 'reset-css';
+import "reset-css";
 import Style from "./App.module.css";
-import "./Assets/portrait-1080p.jpg";
+import bgPortrait from "./Assets/portrait-1080p.jpg";
 import game2048Img from "./Assets/2048-game.jpg";
 import weatherAppImg from "./Assets/weather-app.jpg";
 import worldSolverImg from "./Assets/wordle-solver.jpg";
 import Card from "./Components/Card";
 
+//Styles set programatically so App can be used as sub-module in portfolio
+const htmlStyles = {
+  position: "relative",
+  width: "100vw",
+  height: "100vh",
+};
+
+const bodyStyles = {
+  position: "absolute",
+  inset: 0,
+  backgroundColor: "#000",
+  backgroundImage: `url(${bgPortrait})`,
+  backgroundPosition: "right center",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  color: "#fff",
+  fontSize: "12px",
+  fontFamily: "'Roboto', sans-serif",
+  webkitFontSmoothing: "antialiased",
+  mozOsxFontSmoothing: "grayscale",
+};
+
 export default function App() {
+  //Set HTML and BODY styles programatically
+  for (let key in htmlStyles)
+    document.getElementsByTagName("html")[0].style[key] = htmlStyles[key];
+
+  for (let key in bodyStyles) document.body.style[key] = bodyStyles[key];
+
   return (
     <div className={Style.App}>
       <Card
@@ -25,7 +53,7 @@ export default function App() {
         codeUrl="http://somelink.com"
         previewUrl="http://somelink.com"
       >
-        Weather app text. 
+        Weather app text.
       </Card>
       <Card
         imgSrc={game2048Img}
@@ -34,7 +62,7 @@ export default function App() {
         codeUrl="http://somelink.com"
         previewUrl="/2048"
       >
-        Recreation of 2048 game. 
+        Recreation of 2048 game.
       </Card>
       {/* 2 wedding websites */}
       {/* resume */}
