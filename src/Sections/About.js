5904942ddeded545f7b00d81cf4f0283992544d8
paulcarkner <paul.carkner@gmail.com>
2023-01-20 20:18:10 -0500

    Code clean-up and commenting

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -12,7 +12,25 @@ export default function About() {
           <div>
             <p>
               I've been writing webpages since the &lt;table&gt; element was
-              introduced. 
+              introduced. My first websites were little personal tools and
+              fooling around with different technologies as they came around.
+            </p>
+            <p>
+              I've done full-stack development on multiple websites using
+              ASP.NET C# (MVC), T-SQL, and jQuery hosted on the Azure Cloud.
+              I've also dabbled with SignalR, Serverless Functions, CDN and
+              writing APIs. Being self-employed also meant I did all of the DNS
+              and domain management for all of my websites.
+            </p>
+            <p>
+              Using CSS extensively, wherever possible, is a passion of mine.
+              Why use a 30kB image or complicated animation script when 3 lines
+              of CSS can do the same thing? (Ironically, the animation in the
+              hero of this page is a 10 line script, not CSS.)
+            </p>
+            <p>
+              This past year I have been learning and implimenting React and
+              TypeScript in my projects.
             </p>
           </div>
           <div className={Style.skillContainer}>
